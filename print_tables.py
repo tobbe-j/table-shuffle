f84307418b1639c98019f79a97e3498a36612ee2
Tobias Jern <tobias.jern@aalto.fi>
2018-12-03 22:27:01 +0200

    First version of friend preference - only works for guys who want to sit with guys =)

--- print_tables.py
+++ print_tables.py
@@ -8,7 +8,7 @@ Empty tables will be the same size and have the word empty on them
 def print_tables(tables: dict) -> None:
     for name, table in tables.items():
         print(f"\n{' ' * 30}{name}\n\n")
-        if len(table[0][0]) is 0:
+        if len(table[0][0].name) is 0:
             print("""                        +--------------+
                         |      E       |
                         |      M       |
@@ -22,14 +22,17 @@ def print_tables(tables: dict) -> None:
 
         for idx, row in enumerate(table):
             if idx is 0:
-                pad = 20 - len(row[0])
-                print(f"{' ' * pad}{row[0]}    +---------------+    {row[-1]}")
+                pad = 20 - len(row[0].name)
+                print(f"{' ' * pad}{row[0].name}    +---------------+"
+                      f"    {row[-1].name}")
                 print(f"{' ' * 24}|{' ' * 15}|")
             elif idx is (len(table)-1):
-                pad = 20 - len(row[0])
-                print(f"{' ' * pad}{row[0]}    +---------------+    {row[-1]}")
+                pad = 20 - len(row[0].name)
+                print(f"{' ' * pad}{row[0].name}    +---------------+"
+                      f"    {row[-1].name}")
                 print("\n\n\n")
             else:
-                pad = 20 - len(row[0])
-                print(f"{' ' * pad}{row[0]}    |               |    {row[-1]}")
+                pad = 20 - len(row[0].name)
+                print(f"{' ' * pad}{row[0].name}    |               |"
+                      f"    {row[-1].name}")
                 print(f"{' ' * 24}|{' ' * 15}|")
--- table-shuffle.py
+++ table-shuffle.py
@@ -1,9 +1,10 @@
 import argparse
 import sys
 from random import shuffle
-import readchar
 import pandas as pd
+from itertools import zip_longest
 from print_tables import print_tables
+from person import Person, Empty
 
 
 def getArgs(argvals=None):
@@ -40,22 +41,23 @@ def get_tables():
     return tables
 
 
-def define_sex(names: list, sexes: list) -> dict:
-    if sexes is not None:
-        people_s = list(zip(names, sexes))
-        women = [x[0] for x in list(people_s) if x[1] is 'w']
-        men = [x[0] for x in list(people_s) if x[1] is not 'w']
-        print(len(women) + len(men))
-        names = []
-    else:
-        women = []
-        men = []
-    for name in names:
-        print(f"Is {name} a man/Woman? (M/w) ")
-        if readchar.readchar() is 'w':
-            women.append(name)
-        else:
-            men.append(name)
+# def define_sex(names: list, sexes: list) -> list:
+#     if sexes is not None:
+#         people_s = list(zip(names, sexes))
+#         women = [x[0] for x in list(people_s) if x[1] is 'w']
+#         men = [x[0] for x in list(people_s) if x[1] is not 'w']
+#         print(len(women) + len(men))
+#         names = []
+#     else:
+#         women = []
+#         men = []
+#     for name in names:
+#         print(f"Is {name} a man/Woman? (M/w) ")
+#         if readchar.readchar() is 'w':
+#             women.append(name)
+#         else:
+#             men.append(name)
+def balance_sex(men: list, women: list):
     if len(men) > len(women):
         difference = (len(men) - len(women)) // 2
         women.extend(men[-difference:])
@@ -64,63 +66,86 @@ def define_sex(names: list, sexes: list) -> dict:
         difference = len(women) - len(men)
         men.extend(women[-difference:])
         del women[-difference:]
-    return {'men': men, 'women': women}
+    return [men, women]
 
 
-def randomize_tables(tables: dict, sexes=None) -> dict:
-    people = define_sex(names, sexes)
-    [shuffle(v) for (k, v) in people.items()]
+def randomize_tables(tables: dict, people: list) -> dict:
+    men, women = [], []
+    for item in people:
+        (men if item.sex is 'm' else women).append(item)
+    balance_sex(men, women)
+    shuffle(men)
+    shuffle(women)
+    for person in people:
+        if person.preference is not None:
+            add_friend(men, women, person)
+    people = list(zip_longest(men, women, fillvalue=Empty()))
     for table, size in tables.items():
         tables[table] = []
         for seat in range(size):
-            pair = []
-            if seat % 2 is 0:
-                if people['men']:
-                    pair.append(people['men'].pop(0))
-                else:
-                    pair.append(' ')
-                if people['women']:
-                    pair.append(people['women'].pop(0))
-                else:
-                    pair.append(' ')
+            pair = ()
+            if len(people) is 0:
+                pair = (Empty(), Empty())
             else:
-                if people['women']:
-                    pair.append(people['women'].pop(0))
+                if seat % 2 is 0:
+                    pair = people.pop(0)
                 else:
-                    pair.append(' ')
-                if people['men']:
-                    pair.append(people['men'].pop(0))
-                else:
-                    pair.append(' ')
+                    pair = people.pop(0)[::-1]
             tables[table].append(pair)
 
     return tables
 
 
-def add_friend(men: list, women: list, friend: str) -> None:
-    pass
+def add_friend(men: list, women: list, host: tuple) -> None:
+    print(f"Adding friend to {host}")
+    friend = {x.name: x for x in men + women}.get(host.preference, "")
+    if type(friend) is str:
+        print("returning")
+        return
+    print(friend)
+    if host.sex is friend.sex:
+        if host.sex is 'm':
+            swap_list = men
+        else:
+            swap_list = women
+        host_index = swap_list.index(host)
+        friend_index = swap_list.index(friend)
+        if host_index + 1 < len(swap_list):
+            print(f"Moving {friend.name} to place {host_index + 1} where now"
+                  f"sits {swap_list[host_index + 1].name}")
+            (swap_list[friend_index],
+             swap_list[host_index + 1]) = (swap_list[host_index + 1],
+                                           swap_list[friend_index])
+        else:
+            print(f"Moving {friend.name} to place {host_index + 1}")
+            (swap_list[friend_index],
+             swap_list[host_index - 1]) = (swap_list[host_index - 1],
+                                           swap_list[friend_index])
+    print(men + women)
 
 
 if __name__ == '__main__':
     args = getArgs()
     data = args.file
     names = args.column
-    if args.sexes is None:
-        sexes = None
-    else:
-        sexes = args.sexes
-
+    sexes = args.sexes
     preference = args.preference
+
     try:
         df = pd.read_csv(data)
     except FileNotFoundError:
         print("Could not find file, exiting")
         sys.exit()
     try:
-        names = df[names]
+        df['names'] = df[names]
         if sexes is not None:
-            sexes = df[sexes]
+            df['sexes'] = df[sexes]  # define_sex(names, df[sexes])
+        else:
+            df['sexes'] = None  # TODO: define sexes
+        if preference is not None:
+            df['preference'] = df[preference]
     except KeyError as ke:
         print(f"Could not find column {ke.args}, exiting")
         sys.exit()
-    print_tables(randomize_tables(get_tables(), sexes))
+    people = [Person(i, df) for i in range(len(df.index))]
+    print_tables(randomize_tables(get_tables(), people))
